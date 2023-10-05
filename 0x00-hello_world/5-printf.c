2844e8b0cdad29e7d2e6696c1953563761a7024b
userkw <elkis.kawthar11@gmail.com>
2023-10-05 15:38:58 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a message with proper grammar.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
