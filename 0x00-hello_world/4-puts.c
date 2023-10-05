23e5c428e28d4eb67163a179664d1025e7409e25
userkw <elkis.kawthar11@gmail.com>
2023-10-05 15:24:59 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle\n");
+printf("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
