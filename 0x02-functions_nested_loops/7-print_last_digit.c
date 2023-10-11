79fbc6aa70f2c5414e581b413aa00aae203a3378
userkw <elkis.kawthar11@gmail.com>
2023-10-11 15:20:12 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,12 @@
 *
 * Return: The value of the last digit.
 */
-int print_last_digit(int m)
+int print_last_digit(int n)
 {
-int last_digit;    
-last_digit = m % 10;
+int last_digit;
+last_digit = n % 10;
 if (last_digit < 0)
 last_digit = -last_digit;
 _putchar('0' + last_digit);
-return last_digit;
+return (last_digit);
 }
