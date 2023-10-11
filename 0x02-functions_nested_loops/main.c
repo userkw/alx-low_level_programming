5d136c84d3b2706d813637798a360d5627211551
userkw <elkis.kawthar11@gmail.com>
2023-10-11 14:10:17 -0700

    done

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
