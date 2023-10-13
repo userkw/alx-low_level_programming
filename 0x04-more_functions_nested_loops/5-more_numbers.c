4e51a8a2f99dd1ccccf5b52dcd01523b645cd751
userkw <elkis.kawthar11@gmail.com>
2023-10-13 08:27:28 -0700

    defaulr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-    int i, num;
+	int i, num;
 
-    for (i = 0; i < 10; i++) // Repeat 10 times
-    {
-        for (num = 0; num <= 14; num++)
-        {
-            if (num > 9)
-            {
-                _putchar(num / 10 + '0'); // Print the tens digit
-            }
-            _putchar(num % 10 + '0'); // Print the ones digit
-        }
-        _putchar('\n'); // New line after each iteration
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
