97e0fe355bc46845f7024fb553edef94d384dbc7
userkw <elkis.kawthar11@gmail.com>
2023-10-13 08:04:00 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
 /**
  * print_numbers - prints numbers from 0 to 9
  */
 void print_numbers(void)
 {
-	char digit = '0';
+    char digit = '0';
 
-	while (digit <= '9')
-	{
-		_putchar(digit);
-		digit++;
-	}
-	_putchar('\n');
+    while (digit <= '9')
+    {
+        _putchar(digit);
+        digit++;
+    }
+    _putchar('\n');
 }
