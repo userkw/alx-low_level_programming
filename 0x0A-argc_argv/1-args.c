aafae92e8a68032128e8185a0de2581168598fff
userkw <elkis.kawthar11@gmail.com>
2023-10-27 18:40:56 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,10 +6,8 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{ (void)argv;
-
-    printf("%d\n", argc - 1);
-
-    return (0);
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
 }
