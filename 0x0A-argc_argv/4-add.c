8b8ddd9f5305895df6f8b86d7a6d6b919a086575
userkw <elkis.kawthar11@gmail.com>
2023-10-28 14:28:48 -0700

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - Check if a string contains only digits
+ * @str: Input string
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise
+ */
+int check_num(const char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+        {
+            return (0);
+        }
+    }
+    return (1);
+}
+
+/**
+ * main - Sum the valid numeric command-line arguments
+ * @argc: Argument count
+ * @argv: Argument values
+ *
+ * Return: 0 for success, 1 for errors
+ */
+int main(int argc, char *argv[])
+{
+    int total = 0;
+    int i;
+
+    for (i = 1; i < argc; i++)
+    {
+        if (check_num(argv[i]))
+        {
+            int str_to_int = atoi(argv[i]);
+            total += str_to_int;
+        }
+        else
+        {
+            printf("Error\n");
+            return (1);
+        }
+    }
+
+    printf("%d\n", total);
+
+    return (0);
+}
