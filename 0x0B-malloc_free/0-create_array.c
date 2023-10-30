9e1a216c274da580361add75d345390f171ed720
userkw <elkis.kawthar11@gmail.com>
2023-10-30 13:44:33 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,22 +8,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array_c;
-    unsigned int j;
+	char *array_c;
+	unsigned int j;
 
-    if (size == 0) {
-        return NULL;
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    array_c = (char *)malloc(size * sizeof(char));
+	array_c = (char *)malloc(size * sizeof(char));
 
-    if (array_c == NULL) {
-        return NULL;
-    }
+	if (array_c == NULL)
+	{
+		return (NULL);
+	}
 
-    for (j = 0; j < size; j++) {
-        array_c[j] = c;
-    }
+	for (j = 0; j < size; j++)
+	{
+		array_c[j] = c;
+	}
 
-    return array_c;
+	return (array_c);
 }
