c444a60f02ab0f4033bc025502bb157b15293cc1
userkw <elkis.kawthar11@gmail.com>
2023-10-31 14:47:10 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,23 +7,25 @@
  *
  * Return: grid pointer if successful, else return NULL.
  */
-int **alloc_grid(int width, int height) {
+int **alloc_grid(int width, int height)
+{
 	int **g;
 	int a, d;
 
-	if (width <= 0 || height <= 0) {
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	}
 
 	g = (int **)malloc(height * sizeof(int *));
-	if (g == NULL) {
+	if (g == NULL)
 		return (NULL);
-	}
 
-	for (a = 0; a < height; a++) {
+	for (a = 0; a < height; a++)
+	{
 		g[a] = (int *)malloc(width * sizeof(int));
-		if (g[a] == NULL) {
-			while (a >= 0) {
+		if (g[a] == NULL)
+		{
+			while (a >= 0)
+			{
 				free(g[a]);
 				a--;
 			}
@@ -31,9 +33,8 @@ int **alloc_grid(int width, int height) {
 			return (NULL);
 		}
 
-		for (d = 0; d < width; d++) {
+		for (d = 0; d < width; d++)
 			g[a][d] = 0;
-		}
 	}
 
 	return (g);
