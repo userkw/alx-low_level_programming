aec53d22d1dc3126616da7d37d46c3cd71c087d8
userkw <elkis.kawthar11@gmail.com>
2023-10-31 14:40:33 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,37 +5,36 @@
  * @width: Width of the grid.
  * @height: Height of the grid.
  *
- * Return: A pointer to a newly created grid filled with zeros.
- *         NULL on failure or if width or height is non-positive.
+ * Return grid pointer if successful, else return NULL
  */
 int **alloc_grid(int width, int height) {
-    int **g;
-    int a, d;
+	int **g;
+	int a, d;
 
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
+	if (width <= 0 || height <= 0) {
+		return (NULL);
+	}
 
-    g = (int **)malloc(height * sizeof(int *));
-    if (g == NULL) {
-        return NULL;
-    }
+	g = (int **)malloc(height * sizeof(int *));
+	if (g == NULL) {
+		return (NULL);
+	}
 
-    for (a = 0; a < height; a++) {
-        g[a] = (int *)malloc(width * sizeof(int));
-        if (g[a] == NULL) {
-            while (a >= 0) {
-                free(g[a]);
-                a--;
-            }
-            free(g);
-            return NULL;
-        }
+	for (a = 0; a < height; a++) {
+		g[a] = (int *)malloc(width * sizeof(int));
+		if (g[a] == NULL) {
+			while (a >= 0) {
+				free(g[a]);
+				a--;
+			}
+			free(g);
+			return (NULL);
+		}
 
-        for (d = 0; d < width; d++) {
-            g[a][d] = 0;
-        }
-    }
+		for (d = 0; d < width; d++) {
+			g[a][d] = 0;
+		}
+	}
 
-    return g;
+	return (g);
 }
