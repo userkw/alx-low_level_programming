4297bfe0ccdcc5a32759f863ca356a29cfef83ad
userkw <elkis.kawthar11@gmail.com>
2023-11-06 17:15:35 -0800

    done

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int ss;
+
+    ss = SUM(98, 1024);
+    printf("%d\n", ss);
+    return (0);
+}
