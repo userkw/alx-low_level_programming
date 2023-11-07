781de753d068da7961a145c23951cc19304793dd
userkw <elkis.kawthar11@gmail.com>
2023-11-07 13:30:03 -0800

    done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints information about a dog
+ * @d: Pointer to the struct dog
+ */
+void print_dog(struct dog *d)
+{
+    if (d != NULL)
+    {
+        printf("Name: %s\n", d->name ? d->name : "(nil)");
+        printf("Age: %.6f\n", d->age);
+        printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+    }
+}
