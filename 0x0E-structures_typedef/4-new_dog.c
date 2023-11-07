f0140d587fbf9ff9823598d73fc65567bdc57d6e
userkw <elkis.kawthar11@gmail.com>
2023-11-07 13:46:35 -0800

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,35 +8,36 @@
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
- * Return: A pointer to the new dg or NULL
+ * Return: A pointer to the new dog or NULL 
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *nw_dg_pt;
-    char *nm_cp, *owner_cp;
+	/* Use tabs for indentation */
+	dog_t *nw_dg_pt;
+	char *nm_cp, *owner_cp;
 
-    nw_dg_pt = malloc(sizeof(dog_t));
+	nw_dg_pt = malloc(sizeof(dog_t));
 
-    if (nw_dg_pt == NULL)
-        return (NULL);
+	if (nw_dg_pt == NULL)
+		return (NULL);
 
-    nm_cp = malloc(strlen(name) + 1);
-    owner_cp = malloc(strlen(owner) + 1);
+	nm_cp = malloc(strlen(name) + 1);
+	owner_cp = malloc(strlen(owner) + 1);
 
-    if (nm_cp == NULL || owner_cp == NULL)
-    {
-        free(nm_cp);
-        free(owner_cp);
-        free(nw_dg_pt);
-        return (NULL);
-    }
+	if (nm_cp == NULL || owner_cp == NULL)
+	{
+		free(nm_cp);
+		free(owner_cp);
+		free(nw_dg_pt);
+		return (NULL);
+	}
 
-    strcpy(nm_cp, name);
-    strcpy(owner_cp, owner);
+	strcpy(nm_cp, name);
+	strcpy(owner_cp, owner);
 
-    nw_dg_pt->name = nm_cp;
-    nw_dg_pt->age = age;
-    nw_dg_pt->owner = owner_cp;
+	nw_dg_pt->name = nm_cp;
+	nw_dg_pt->age = age;
+	nw_dg_pt->owner = owner_cp;
 
-    return (nw_dg_pt);
+	return (nw_dg_pt);
 }
