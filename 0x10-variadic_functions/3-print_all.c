792b6c4da11f2ad31f2e94a62e91d6d43df2b662
userkw <elkis.kawthar11@gmail.com>
2023-11-09 12:58:09 -0800

    done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,56 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all - Prints anything.
- * @format: A list of types of arguments passed to the function.
+ * print_all - Prints anything based on the format provided.
+ * @format: A string containing the types of arguments passed to the function.
  *          c: char
  *          i: integer
  *          f: float
- *          s: char * if the string is NULL, print nil instead
- *          any other char should be ignored
+ *          s: char * (if the string is NULL, print (nil) instead)
+ *          Any other char should be ignored.
  */
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	unsigned int a = 0;
-	char *str;
-	int num;
-	double fnum;
-	char *separator = "";
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char separator = ',';
 
-	va_start(arg, format);
+    va_start(args, format);
 
-	while (format && format[a])
-	{
-		switch (format[a])
-		{
-			case 'c':
-				printf("%s%c", separator, va_arg(arg, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(arg, int));
-				break;
-			case 'f':
-				fnum = va_arg(arg, double); 
-				printf("%s%f", separator, fnum);
-				break;
-			case 's':
-				str = va_arg(arg, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", separator, str);
-				break;
-			default:
-				a++;
-				continue;
-		}
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+        case 'c':
+            printf("%c", va_arg(args, int));
+            break;
+        case 'i':
+            printf("%d", va_arg(args, int));
+            break;
+        case 'f':
+            printf("%f", va_arg(args, double));
+            break;
+        case 's':
+            str = va_arg(args, char *);
+            if (str == NULL)
+                str = "(nil)";
+            printf("%s", str);
+            break;
+        default:
+            separator = '\0';
+            break;
+        }
 
-		separator = ", ";
-		a++;
-	}
+        if (format[i + 1] && separator != '\0')
+            printf("%c ", separator);
 
-	printf("\n");
-	va_end(arg);
+        i++;
+    }
+
+    printf("\n");
+
+    va_end(args);
 }
