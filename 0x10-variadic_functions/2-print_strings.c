880b78c5f97a0b29839adb32911ee41b0a3bba68
userkw <elkis.kawthar11@gmail.com>
2023-11-09 12:42:57 -0800

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,9 +1,9 @@
-#include "custom_variadic_functions.h"
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
 /**
- * print_custom_strings - Print strings with a specified separator.
+ * print_strings - Print strings with a specified separator.
  * @sep: The separator to be printed between strings.
  * @num_strings: The number of strings passed to the function.
  * @...: A variable number of strings to be printed.
@@ -11,7 +11,7 @@
  * Description: If sep is NULL, it is not printed.
  *              If one of the strings is NULL, (nil) is printed instead.
  */
-void print_custom_strings(const char *sep, const unsigned int num_strings, ...)
+void print_strings(const char *sep, const unsigned int num_strings, ...)
 {
 	va_list args;
 	char *current_str;
