9e6b08ebdbf5acf208f045dad7303c64b1dd4451
usekw <elkis.kawthar11.com>
2023-11-17 07:27:46 -0800

    done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - returns the number of elms
+ * @h: pointer
  *
- * Return: number of elements in h
+ * Return: number
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t a = 0;
 
 	while (h)
 	{
-		n++;
+		a++;
 		h = h->next;
 	}
-	return (n);
+	return (a);
 }
