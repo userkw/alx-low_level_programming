11aecc5136d43e24cb5211cdab53bd29a699a76a
usekw <elkis.kawthar11.com>
2023-11-21 11:44:42 -0800

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,66 +2,61 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /**
- * check_num - check - string
+ * check_num - checks if a string consists of digits
+ * @str: the input string to be checked
  *
- * Return: Always 0
+ * Return: 1 if the string consists of digits, 0 otherwise
  */
 int check_num(char *str)
 {
-	unsigned int cont;
+    unsigned int cont;
 
-	cont = 0;
-	while (cont < strlen(str))
+    cont = 0;
+    while (cont < strlen(str))
+    {
+        if (!isdigit(str[cont]))
+        {
+            return (0);
+        }
 
-	{
-		if (!isdigit(str[cont]))
-		{
-			return (0);
-		}
-
-		cont++;
-	}
-	return (1);
+        cont++;
+    }
+    return (1);
 }
 
 /**
- * main - Print the name of the program
- * @argc: Count argts
- * @argv: Args
+ * main - Print the sum of command line arguments that are numbers
+ * @argc: Count of command line arguments
+ * @argv: Array of command line argument strings
  *
- * Return: 0
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
-
 {
+    int count;
+    int str_to_int;
+    int summ = 0;
 
-	int count;
-	int str_to_int;
-	int summ = 0;
+    count = 1;
+    while (count < argc)
+    {
+        if (check_num(argv[count]))
+        {
+            str_to_int = atoi(argv[count]);
+            summ += str_to_int;
+        }
+        else
+        {
+            printf("Error\n");
+            return (1);
+        }
 
-	count = 1;
-	while (count < argc)
-	{
-		if (check_num(argv[count]))
+        count++;
+    }
 
-		{
-			str_to_int = atoi(argv[count]);
-			summ += str_to_int;
-		}
+    printf("%d\n", summ);
 
-		
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		count++;
-	}
-
-	printf("%d\n", summ);
-
-	return (0);
+    return (0);
 }
