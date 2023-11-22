3a623c1fcce944442907a8eb1627b8d50af934bc
usekw <elkis.kawthar11.com>
2023-11-22 00:42:09 -0800

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,40 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * main - sop
- * @argc:argc
- * @argv:argv
- *
- * Return: 1 or 0
+ * main - prints all arguments it receives
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i, result;
+	int i;
 
-	result = 0;
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		char *j;
-
-		for (j = argv[i]; *j; j++)
-		{
-			if (*j < '0' || *j  > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-
-		result += atoi(argv[i]);
-	}
-	printf("%d\n", result);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
