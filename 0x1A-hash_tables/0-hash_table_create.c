9cc90c051adc7928d7d353cdb7d46f32027ec9d1
userkw <elkis.kawthar11@gmail.com>
2024-02-24 12:07:44 -0800

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *h_tble;
-    unsigned long int ae;
+	hash_table_t *h_tble;
+	unsigned long int ae;
 
-    h_tble = malloc(sizeof(hash_table_t));
-    if (h_tble == NULL)
-        return (NULL);
-    h_tble->size = size;
-    h_tble->array = malloc(size * sizeof(hash_node_t *));
-    if (h_tble->array == NULL)
-    {
-        free(h_tble);
-        return (NULL);
-    }
-    for (ae = 0; ae < size; ae++)
-        h_tble->array[ae] = NULL;
-    return (h_tble);
+	h_tble = malloc(sizeof(hash_table_t));
+	if (h_tble == NULL)
+		return (NULL);
+	h_tble->size = size;
+	h_tble->array = malloc(size * sizeof(hash_node_t *));
+	if (h_tble->array == NULL)
+	{
+		free(h_tble);
+		return (NULL);
+	}
+	for (ae = 0; ae < size; ae++)
+		h_tble->array[ae] = NULL;
+	return (h_tble);
 }
